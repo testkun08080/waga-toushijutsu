1a4a095845c13b3599ec9f45f479e6dfa0119964
testkun <154046906+testkun08080@users.noreply.github.com>
2025-09-28 17:11:34 +0700

    update PWA

--- stock_serch/vite.config.ts
+++ stock_serch/vite.config.ts
@@ -1,10 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB に拡大
+      },
+      manifest: {
+        name: "yf我が投資術",
+        short_name: "yf我が投資術",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#1d4ed8",
+        icons: [
+          {
+            src: "/favicon.ico",
+            sizes: "64x64",
+            type: "image/ico",
+          },
+        ],
+      },
+      registerType: "autoUpdate",
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true,
+      },
+    }),],
   base: process.env.NODE_ENV === 'production' ? '/waga-toushijutsu/' : '/',
   build: {
     outDir: 'dist',
@@ -13,7 +41,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'],
+          vendor: ['react', 'react-dom', 'react-router-dom'],
           papaparse: ['papaparse']
         }
       }
